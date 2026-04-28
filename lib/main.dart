53a40bdaa4b86211fefd92d2f9aaacac5a376b99
Oxygen12-cyber <adeolaodunjo32@gmail.com>
2026-04-28 01:29:02 +0100

    refactor: remove device_preview_plus dependency and clean up main app configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:josh_wpblog/pages/homepage.dart';
-import 'package:device_preview_plus/device_preview_plus.dart';
 
 void main() {
-  runApp(DevicePreview(builder: (context) => const MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,16 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // home: NewsPage(
-      //   imageUrl:
-      //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq_ZUCVzfKJZ7J6PfhIDjoKqvJwTTDp7u_NQ&s",
-      //   title: "Avatar: The last Airbender and the \nlast few things to do",
-      //   content:
-      //       "    random content in this place around 60 words to fill up the space in this application. Embark on a globetrotting quest to fulfil Aang’s destiny with all your favorite characters and companions including Aang, Katara, Sokka, Toph, Suki and Zuko and the much-loved Appa and Momo in Avatar: Generations, an open-world RPG based on the animated hit Avatar the Last Airbender. Embark on a globetrotting quest to fulfil Aang’s destiny with all your favorite characters and companions including Aang, Katara, Sokka, Toph, Suki and Zuko and the much-loved Appa and Momo in Avatar: Generations, an open-world RPG based on the animated hit Avatar the Last Airbender.Embark on a globetrotting quest to fulfil Aang’s destiny with all your favorite characters and companions including Aang, Katara, Sokka, Toph, Suki and Zuko and the much-loved Appa and Momo in Avatar: Generations, an open-world RPG based on the animated hit Avatar the Last Airbender.Embark on a globetrotting quest to fulfil Aang’s destiny with all your favorite characters and companions including Aang, Katara, Sokka, Toph, Suki and Zuko and the much-loved Appa and Momo in Avatar: Generations, an open-world RPG based on the animated hit Avatar the Last Airbender.Embark on a globetrotting quest to fulfil Aang’s destiny with all your favorite characters and companions including Aang, Katara, Sokka, Toph, Suki and Zuko and the much-loved Appa and Momo in Avatar: Generations, an open-world RPG based on the animated hit Avatar the Last Airbender.Embark on a globetrotting quest to fulfil Aang’s destiny with all your favorite characters and companions including Aang, Katara, Sokka, Toph, Suki and Zuko and the much-loved Appa and Momo in Avatar: Generations, an open-world RPG based on the animated hit Avatar the Last Airbender.Embark on a globetrotting quest to fulfil Aang’s destiny with all your favorite characters and companions including Aang, Katara, Sokka, Toph, Suki and Zuko and the much-loved Appa and Momo in Avatar: Generations, an open-world RPG based on the animated hit Avatar the Last Airbender.Embark on a globetrotting quest to fulfil Aang’s destiny with all your favorite characters and companions including Aang, Katara, Sokka, Toph, Suki and Zuko and the much-loved Appa and Momo in Avatar: Generations, an open-world RPG based on the animated hit Avatar the Last Airbender.",
-      // ),
-      home: HomePage(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
